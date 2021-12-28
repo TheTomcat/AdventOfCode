bb81a46f7064bd69e924a0b58e6c5e0cc450e102
Thomas Vos <vossy01@gmail.com>
2021-12-28 19:13:13 +1000

    24

--- year_2021/day_24_2021.py
+++ year_2021/day_24_2021.py
@@ -60,6 +60,26 @@ class ALU:
     def __repr__(self):
         return f'ALU({self.stack})'                 
                     
+class TranslateALU:
+    def __init__(self, instructions, inp):
+        self.raw_instructions = instructions
+        self.stack = {'w':0,'x':0,'y':0,'z':0}
+        self.pyth = []
+        for instr in self.raw_instructions:
+            match instr:
+                case ["inp", var]:
+                    self.pyth.append(f"{var}={inp.pop(0)}")
+                case [command, var1, var2]:
+                    if command == "add":
+                        self.pyth.append(f"{var1}={var1}+{var2}")
+                    elif command == "mul":
+                        self.pyth.append(f"{var1}={var1}*{var2}")
+                    elif command == "div":
+                        self.pyth.append(f"{var1}=int({var1}/{var2})")
+                    elif command == "mod":
+                        self.pyth.append(f"{var1}={var1}%{var2}")
+                    elif command == "eql":
+                        self.pyth.append(f"{var1}=1 if {var1} == {var2} else 0")             
 
 @solution_timer(2021,24,1)
 def part_one(data: List[str]):
